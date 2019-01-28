c9411c3039300f9b49691693220778e3dfed9912
Eric (Xiaoxing) <40647952+JacePhoenix@users.noreply.github.com>
2019-01-28 16:19:57 -0600

    update publisher.py

--- Assignment1/publisher.py
+++ Assignment1/publisher.py
@@ -16,6 +16,6 @@ socket.bind("tcp://" + IPAddr + ":"+ port)
 while True:
     topic = random.randrange(9999,10005)
     messagedata = random.randrange(1,215) - 80
-    print "%d %d" % (topic, messagedata)
-    socket.send("%d %d" % (topic, messagedata))
+    print (topic, messagedata)
+    socket.send((topic, messagedata))
     time.sleep(1)
