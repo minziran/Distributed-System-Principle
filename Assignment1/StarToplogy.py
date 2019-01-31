774bd917d4a785beda1b2d33cf2d97370beaa42a
canadyre <33406897+canadyre@users.noreply.github.com>
2019-01-30 19:26:17 -0600

    Update StarToplogy.py

--- Assignment1/StarToplogy.py
+++ Assignment1/StarToplogy.py
@@ -6,16 +6,30 @@ from mininet.log import setLogLevel
 
 class BusToplogy:
 	"Linear topology of k switches, with one host per switch."
-	def __init__(self, j, l, **opts):
+	def __init__(self, pub, sub, b, **opts):
 
 	super(LinearTopo, self).__init__(**opts)
-	k = j + l
-	self.k = k
+	self.pub = pub
+	self.sub = sub
+	self.b = b
 	lastSwitch = None
-	for i in irange(1, k):
-		host = self.addHost('h%s' % i)
+	for i in irange(1, pub):
+		host = self.addHost('pub%s' % i)
 		switch = self.addSwitch('s%s' % i)
 		self.addLink( host, switch)
-		if lastSwitch:
-			self.addLink( switch, lastSwitch)
-		lastSwitch = switch
+		##if lastSwitch:
+		##	self.addLink( switch, lastSwitch)
+		##lastSwitch = switch
+	for i in irange(1, sub):
+		host = self.addHost('sub%s' % i)
+		switch = self.addSwitch('s%s' % i)
+		self.addLink( host, switch)
+		##if lastSwitch:
+		##	self.addLink( switch, lastSwitch)
+		##lastSwitch = switch
+	host = self.addhost('b%s' % len(b))
+	switch = self.addSwitch('s%s' % len(b))
+	self.addlink(host, switch)
+	if lastSwitch:
+		self.addLink( switch, lastSwitch)
+	lastSwitch = switch
