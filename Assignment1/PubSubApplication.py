e34dac9e4ed1b2e3077e9e96c2f951de0d1a47ac
Ziran Min <minziran19950716@163.com>
2019-01-30 19:21:49 -0600

    Add files via upload

--- Assignment1/PubSubApplication.py
+++ Assignment1/PubSubApplication.py
@@ -10,15 +10,11 @@ from BusTopology import BusTopology
 from StarTopology import StarTopology
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import CPULimitedHost
 from mininet.link import TCLink
-from mininet.net import CLI
 from mininet.util import dumpNodeConnections
-from mininet.log import setLogLevel, info
-from mininet.util import pmonitor
 
-def create_net(topo, test_command, pub_list, sub_list, broker):
 
+def create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, broker):
 
     net = Mininet(topo=topo, link=TCLink)
     net.start()
@@ -37,18 +33,18 @@ def create_net(topo, test_command, pub_list, sub_list, broker):
 
     def call_broker():
        broker.cmd('xterm -e python ZMQ_broker.py 5556 5557')
-    def call_publisher():
-        pub.cmd('xterm -e python ZMQ_publisher.py  ' + str(broker_ip) + str(test_command))
-    def call_subscriber():
-        sub.cmd('xterm -e python ZMQ_subscriber.py  ' + str(broker_ip) + str(test_command))
+    def call_publisher(pub_command):
+        pub.cmd('xterm -e python ZMQ_publisher.py  ' + str(broker_ip) + str(pub_command))
+    def call_subscriber(sub_command):
+        sub.cmd('xterm -e python ZMQ_subscriber.py  ' + str(broker_ip) + str(sub_command))
 
     threading.Thread(target=call_broker, args=()).start()
     time.sleep(3)
-    for pub in pub_list:
-        threading.Thread(target=call_publisher, args=()).start()
+    for k, pub in enumerate(pub_list):
+        threading.Thread(target=call_publisher(pub_command_list[k]), args=()).start()
         time.sleep(5)
-    for sub in sub_list:
-        threading.Thread(target=call_subscriber, args=()).start()
+    for k, sub in enumerate(sub_list):
+        threading.Thread(target=call_subscriber(sub_command_list[k]), args=()).start()
         time.sleep(5)
 
     try :
@@ -64,15 +60,20 @@ def main():
     pub_num = 3
     sub_num = 3
     topo_type = 'bus'
-    test_command = ''
+    pub_command_list =[]
+    sub_command_list = []
+
+
+
+
     pub_list, sub_list = [], []
     broker = None
     if topo_type == 'bus':
         topo = BusTopology(pub_num,sub_num)
-        create_net(topo, test_command, pub_list, sub_list, broker)
+        create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, broker)
     elif topo_type == 'star':
         topo = StarTopology(pub_num,sub_num)
-        create_net(topo, test_command, pub_list, sub_list, broker)
+        create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, broker)
     else:
         exit("Could not find mached topology. 'bus' and 'star' are provided")
 
