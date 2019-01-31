39b647fa4ee8677d8cf2fd07d9a1dfd9442d160b
canadyre <33406897+canadyre@users.noreply.github.com>
2019-01-30 18:42:35 -0600

    Add files via upload

---
+++ Assignment1/StarToplogy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import irange,dumpNodeConnections
+from mininet.log import setLogLevel
+
+class BusToplogy:
+	"Linear topology of k switches, with one host per switch."
+	def __init__(self, j, l, **opts):
+	"""Init.
+	k: number of switches (and hosts)
+	hconf: host configuration options
+	lconf: link configuration options"""
+	super(LinearTopo, self).__init__(**opts)
+	k = j + l
+	self.k = k
+	lastSwitch = None
+	for i in irange(1, k):
+		host = self.addHost('h%s' % i)
+		switch = self.addSwitch('s%s' % i)
+		self.addLink( host, switch)
+		if lastSwitch:
+			self.addLink( switch, lastSwitch)
+		lastSwitch = switch
