b0c745eb618d5dd0fd31cb6355d9e1ba2b329e3c
= <xqiu90@gmail.com>
2019-01-31 14:44:48 -0800

    update

--- Assignment1/BusToplogy.py
+++ Assignment1/BusToplogy.py
@@ -1,49 +1,58 @@
-#!/usr/bin/python
+
 from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.util import irange,dumpNodeConnections
-from mininet.log import setLogLevel
 
-class BusToplogy(Topo):
-	"Linear topology of k switches, with one host per switch."
-	def __init__(self, pub, sub, b, **opts):
 
-		super(LinearTopo, self).__init__(**opts)
-		self.pub = pub
-		self.sub = sub
-		self.b = b
-	
-		lastSwitch = None
-		for i in irange(1, pub):
-			host = self.addHost('pub%s' % i)
-			switch = self.addSwitch('s%s' % i)
-			self.addLink( host, switch)
-			##if lastSwitch:
-			##	self.addLink( switch, lastSwitch)
-			##lastSwitch = switch
-		for i in irange(1, sub):
-			host = self.addHost('sub%s' % i)
-			switch = self.addSwitch('s%s' % i+pub)
-			self.addLink( host, switch)
-			##if lastSwitch:
-			##	self.addLink( switch, lastSwitch)
-			##lastSwitch = switch
-		host = self.addhost('b%s' % b)
-		switch = self.addSwitch('s%s' % sub+pub+b)
-		self.addlink(host, switch)
-		if lastSwitch:
-			self.addLink( switch, lastSwitch)
-		lastSwitch = switch
+class BusTopology(Topo):
+    # Star topology example.
 
-topos = {'mytopo': (lambda: BusTopology(2, 3, 1))}
-	
-def main():
-    topo = BusTopology(2, 3, 1)
-    net = Mininet(topo = topo, host = CPULimitedHost, link = TCLink)
-    net.start()
-    dumpNodeConnections(net.hosts)
-    net.stop()
+    def __init__(self, _pubNum=None, _subNum=None):
+        "Create custom topo."
 
-if __name__ == '__main__':
-    setLogLevel( 'info' )
-    main()
+        # Initialize topology
+        Topo.__init__( self )
+        self.hostList = list()
+        self.localSwitchList = list()
+        self.busSwitchList = list()
+
+        # track the name of host and switch.
+        index = 1
+        hostNum = _pubNum + _subNum + 1  # a broker at the center
+
+        # Add hosts
+
+        for num in range(0, hostNum):
+            hName = "h" + str(index)
+            self.hostList.append(self.addHost(hName))
+            index += 1
+
+        # Add local switches
+        index = 1
+
+        for num in range(0, hostNum):
+            sName = "s" + str(index)
+            self.localSwitchList.append(self.addSwitch(sName))
+            index += 1
+
+        # Add bus switches
+        index = 1
+        numBusSwitch = (hostNum - 1)//2 + 1
+
+        for num in range(numBusSwitch):
+            sName = "bs" + str(index)
+            self.busSwitchList.append(self.addSwitch(sName))
+            index += 1
+
+        # Build Local Links
+        for num in range(hostNum):
+            self.addLink(self.hostList[num], self.localSwitchList[num])
+
+        # Link the bus switches together
+        for num in range(0, numBusSwitch-1):
+            self.addLink(self.busSwitchList[num], self.busSwitchList[num + 1])
+
+        # Build Links between local switches and bus switches
+        for num in range(hostNum):
+            self.addLink(self.localSwitchList[num], self.busSwitchList[num//2])
+
+
+topos = { 'bustopo': ( lambda: BusTopology(2, 3) ) }
