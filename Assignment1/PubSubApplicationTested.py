fb0f159515543755e37c282c6522c4a87c122b73
= <xqiu90@gmail.com>
2019-02-02 13:04:40 -0800

    update

--- Assignment1/PubSubApplicationTested.py
+++ Assignment1/PubSubApplicationTested.py
@@ -3,7 +3,7 @@ import threading
 
 from BusTopology import BusTopology
 from StarTopology import StarTopology
-from mininet.topo import Topo
+from mininet.cli import CLI
 from mininet.net import Mininet
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
@@ -15,6 +15,7 @@ def create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, bro
     net.start()
     dumpNodeConnections(net.hosts)
     net.pingAll()
+
 
     # Set up the hX as pub or sub or broker
 
@@ -30,6 +31,7 @@ def create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, bro
 
     # TEST: let h7 be the broker; h1, h2, h3 be the publishers; h4, h5, h6 be the subscribers
     broker = net.hosts[6]
+    CLI(net)
 
     for num in range(0, 3):
         pub_list.append(net.hosts[num])
@@ -41,26 +43,35 @@ def create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, bro
 
 
     def call_broker():
-        broker.cmd('xterm -e python ZMQ_broker.py 5556 5557')
+        # broker.cmd('xterm -e python ZMQ_broker.py 5556 5557')
+        broker.cmd('xterm echo hello')
 
     def call_publisher(pub_command):
         pub.cmd('xterm -e python ZMQ_publisher.py  ' + str(broker_IP) + str(pub_command))
+        # pub.cmd('python ZMQ_publisher.py  ' + str(broker_IP) + str(pub_command))
 
     def call_subscriber(sub_command):
         sub.cmd('xterm -e python ZMQ_subscriber.py  ' + str(broker_IP) + str(sub_command))
+        # sub.cmd('python ZMQ_subscriber.py  ' + str(broker_IP) + str(sub_command))
+
 
     threading.Thread(target=call_broker, args=()).start()
     time.sleep(3)
+
     for k, pub in enumerate(pub_list):
         threading.Thread(target=call_publisher(pub_command_list[k]), args=()).start()
         time.sleep(5)
+
     for k, sub in enumerate(sub_list):
         threading.Thread(target=call_subscriber(sub_command_list[k]), args=()).start()
         time.sleep(5)
 
     try :
-        while True:
-            pass
+        time.sleep(1000)
+
+        # while True:
+        #     pass
+
     except Exception as e:
         print(e)
 
@@ -80,8 +91,8 @@ def main():
 
     # Testing echo hello
     for num in range(3):
-        pub_command_list.append("echo hello" + str(num))
-        sub_command_list.append("echo world" + str(num))
+        pub_command_list.append("Hello" )
+        sub_command_list.append("Hello" )
 
     if topo_type == 'bus':
         topo = BusTopology(pub_num,sub_num)
@@ -90,7 +101,9 @@ def main():
         topo = StarTopology(pub_num,sub_num)
         create_net(topo, pub_command_list, sub_command_list, pub_list, sub_list, broker)
     else:
-        exit("Could not find mached topology. 'bus' and 'star' are provided")
+        exit("Could not find matched topology. 'bus' and 'star' are provided")
+
 
 if __name__ == '__main__':
     main()
+
