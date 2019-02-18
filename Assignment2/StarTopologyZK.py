e69a4849e258bd67c84fe5285ec2150c4a9debf5
= <xqiu90@gmail.com>
2019-02-18 13:49:48 -0800

    update

--- Assignment2/StarTopologyZK.py
+++ Assignment2/StarTopologyZK.py
@@ -8,7 +8,7 @@ from mininet.topo import Topo
 class StarTopology(Topo):
     # Star topology example.
 
-    def __init__(self, _pubNum=None, _subNum=None):
+    def __init__(self, _pubNum=None, _subNum=None, _brokerNum=None):
         """Create start topology"""
 
         # Initialize topology
@@ -20,8 +20,12 @@ class StarTopology(Topo):
         if _subNum is None:
             _subNum = 1
 
+        if _brokerNum is None:
+            _brokerNum = 1
+
         self.pubNum = _pubNum
         self.subNum = _subNum
+        self.brokerNum = _brokerNum
 
         # Initialize topology
         Topo.__init__(self)
@@ -31,10 +35,19 @@ class StarTopology(Topo):
 
         # track the name of host and switch.
 
-        hostNum = _pubNum + _subNum + 1     # a broker at the center
+        hostNum = _pubNum + _subNum + _brokerNum     # a broker at the center
 
-        # Add broker at the center
-        hostList.append(self.addHost("broker"))
+        # Add one broker at the center
+        hostList.append(self.addHost("broker1"))
+
+        # The rest brokers are set
+        index = 2
+
+        for num in range(1, _pubNum):
+            brokerName = "brokers" + str(index)
+            hostList.append(self.addHost(brokerName))
+            index += 1
+
 
         # Add publishers
         index = 1
@@ -52,26 +65,29 @@ class StarTopology(Topo):
             hostList.append(self.addHost(subName))
             index += 1
 
-        # Add switches
-        # Add and connect bs0 to the broker
-        switchList.append(self.addSwitch('bs0'))
+        # Add local switches to brokers
+        index = 1
 
+        for num in range(0, _brokerNum):
+            brkSName = "bs" + str(index)
+            switchList.append(self.addSwitch(brkSName))
+            index += 1
 
         # Add local switches to the publisher
         index = 1
         for num in range(0, _pubNum):
-            pubName = "ps" + str(index)
-            switchList.append(self.addSwitch(pubName))
+            pubSName = "ps" + str(index)
+            switchList.append(self.addSwitch(pubSName))
             index += 1
 
         # Add local switches to the publisher
         index = 1
         for num in range(0, _subNum):
-            subName = "ss" + str(index)
-            switchList.append(self.addSwitch(subName))
+            subSName = "ss" + str(index)
+            switchList.append(self.addSwitch(subSName))
             index += 1
 
-        # Form the star topology, let the first host "h0" the broker and put it at the center
+        # Form the star topology, let the first host -- the broker1 and put it at the center
 
         # self.addLink(leftHost, leftSwitch)
         # connect the broker with switch
@@ -86,4 +102,4 @@ class StarTopology(Topo):
             self.addLink(switchList[0], switchList[num])
 
 
-topos = { 'startopo': ( lambda: StarTopology(2, 3) ) }
+topos = { 'startopoZK': ( lambda: StarTopology(3, 3, 3) ) }
