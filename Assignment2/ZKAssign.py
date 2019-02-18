81ec5a99e9cc491f8ad717e491f0456b89be92ab
= <xqiu90@gmail.com>
2019-02-18 13:17:25 -0800

    update

--- Assignment2/ZKAssign.py
+++ Assignment2/ZKAssign.py
@@ -48,11 +48,14 @@ class ZKAssigment(object):
             self.create_nodes(path, data)
 
     def create_nodes(self, path: str = "/test_znode", data: str = None):
-        if data is None:
-            self.zk.create(path, makepath=True)
+        if self.zk.exists(path):
+            return
         else:
-            bData = data.encode("utf-8")
-            self.zk.create(path, bData, makepath=True)
+            if data is None:
+                self.zk.create(path, makepath=True)
+            else:
+                bData = data.encode("utf-8")
+                self.zk.create(path, bData, makepath=True)
 
     def check_exists(self, path: str = "/test_znode") -> bool:
         if self.zk.exists(path):
@@ -88,19 +91,22 @@ class ZKAssigment(object):
     def pubsub_watch(self):
         @self.zk.DataWatch(self.brokers_path)
         def pubsub_watch_handler(bData, status):
-            print("the data is: " + bData.decode("utf-8") + ".\n")
-            print("The status is: " + str(status) + ".\n")
+            if bData:
+                print("The data is: " + bData.decode("utf-8") + ".\n")
+
+            if status:
+                print("The status is: " + str(status) + ".\n")
 
     def stop_kazoo(self):
         self.zk.stop()
 
     def clean_up(self, num):
         for num in range(0, num):
-            test.delete_node(f"/brokers/zbroker{num}")
-            test.delete_node(f"/zsub{num}")
-            test.delete_node(f"/zpub{num}")
+            self.delete_node(f"/brokers/zbroker{num}")
+            self.delete_node(f"/zsub{num}")
+            self.delete_node(f"/zpub{num}")
 
-        test.delete_node("/brokers")
+        self.delete_node("/brokers")
 
     def __del__(self):
         self.zk.stop()
@@ -121,4 +127,3 @@ if __name__ == "__main__":
 
     # clean up
     test.clean_up(3)
-    test.stop_kazoo()
