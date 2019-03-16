2c112b2ffa795095adfd2322d571e0eeaaabfe7b
= <xqiu90@gmail.com>
2019-03-16 17:53:22 -0600

    updaye

--- Assignment3/HistoryQueue.py
+++ Assignment3/HistoryQueue.py
@@ -4,6 +4,7 @@ A class for storing the message history.
 '''
 
 from collections import deque
+from itertools import islice
 
 
 class HistoryQueue:
@@ -17,6 +18,24 @@ class HistoryQueue:
 
         self.msg_queue.appendleft(_text)
 
+    def get_message(self, _num=1):
+        '''
+        A method for get history
+        :param _num:
+        :return: an iterable
+        '''
+        # If the buffer is too small
+        if _num > self.max_size:
+            print("The history buffer is not large enough!")
+            return
+
+        # If the messages are not enough
+        if _num > len(self.msg_queue):
+            print("There is not enough messages!")
+            return
+
+        return islice(self.msg_queue, 0, _num)
+
 
 if __name__ == "__main__":
     test = HistoryQueue(5)
@@ -25,3 +44,5 @@ if __name__ == "__main__":
         test.push_message(i)
 
     print(list(test.msg_queue))
+    print(list(test.get_message(3)))
+    test.get_message(7)
