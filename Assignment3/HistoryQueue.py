944515f58b845bbce85fc46f944a61d56de6e128
EricXQiu <xqiu90@gmail.com>
2019-03-18 15:00:03 -0700

    update

--- Assignment3/HistoryQueue.py
+++ Assignment3/HistoryQueue.py
@@ -5,20 +5,21 @@ A class for storing the message history.
 
 from collections import deque
 from itertools import islice
+from typing import Dict
 
 
-class HistoryQueue:
+class subHistory:
     def __init__(self, _maxSize):
         self.msg_queue = deque()
         self.max_size = _maxSize
 
-    def push_history(self, _text):
+    def push_message(self, _text):
         if len(self.msg_queue) == self.max_size:
             self.msg_queue.pop()
 
         self.msg_queue.appendleft(_text)
 
-    def get_history(self, _num=1):
+    def get_message(self, _num=1):
         '''
         A method for get history
         :param _num: the number of history need to get
@@ -26,23 +27,41 @@ class HistoryQueue:
         '''
         # If the buffer is too small
         if _num > self.max_size:
-            print("The history buffer is not large enough!")
+            print("The history buffer is not large enough!\n")
             return
 
         # If the messages are not enough
         if _num > len(self.msg_queue):
-            print("There is not enough messages!")
+            print("There is not enough messages!\n")
             return
 
         return islice(self.msg_queue, 0, _num)
 
 
-# if __name__ == "__main__":
-#     test = HistoryQueue(5)
-#
-#     for i in range(0, 20):
-#         test.push_history(i)
-#
-#     print(list(test.msg_queue))
-#     print(list(test.get_history(3)))
-#     test.get_history(7)
+class HistoryQueue:
+    def __init__(self, _maxSize):
+        self.topic_history: Dict[str, subHistory] = dict()
+        self.max_size = _maxSize
+
+    def push_history(self, _topic, _message):
+
+        if not(_topic in self.topic_history):
+            self.topic_history[_topic] = subHistory(self.max_size)
+
+        self.topic_history[_topic].push_message(_message)
+
+    def get_history(self, _topic, _history_num):
+        if not(_topic in self.topic_history):
+            print("No history in this topic! \n")
+            return
+        else:
+            return self.topic_history[_topic].get_message(_history_num)
+
+
+if __name__ == "__main__":
+    test = HistoryQueue(5)
+
+    test.push_history('1', '2')
+    print(list(test.get_history('1', 1)))
+    test.get_history('1', 3)
+    test.get_history('2', 1)
